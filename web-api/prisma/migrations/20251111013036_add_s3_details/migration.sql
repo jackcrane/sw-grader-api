365503002eb259bf4c650adf876afc1acb8b4d59
Jack Crane <jack@jackcrane.rocks>
2025-11-10 22:47:43 -0600

    Nuke migrations

--- web-api/prisma/migrations/20251111013036_add_s3_details/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Submission" ADD COLUMN     "fileKey" TEXT,
-ADD COLUMN     "fileUrl" TEXT,
-ADD COLUMN     "screenshotKey" TEXT,
-ADD COLUMN     "screenshotUrl" TEXT;
--- web-api/prisma/migrations/20251111033747_add_signatures/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "AssignmentSignature" DROP CONSTRAINT "AssignmentSignature_assignmentId_fkey";
-
--- AlterTable
-ALTER TABLE "AssignmentSignature" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AddForeignKey
-ALTER TABLE "AssignmentSignature" ADD CONSTRAINT "AssignmentSignature_assignmentId_fkey" FOREIGN KEY ("assignmentId") REFERENCES "Assignment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
